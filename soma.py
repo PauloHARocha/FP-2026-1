6301468937174711e4b3b2f2dda4be808dc2da9b
PauloHARocha <ph.alcantara.rocha@gmail.com>
2026-03-17 08:24:36 -0300

    Add examplo soma

---
+++ soma.py
@@ -0,0 +1,12 @@
+# soma dois numeros
+# 1. recebe primeiro numero do usuario
+# 2. recebe o segundo numero do usuario
+# 3. soma os dois numeros
+# 4. imprime a soma
+
+num01 = float(input("Digite o primeiro numero:"))
+num02 = float(input("Digite o segundo numero:"))
+
+soma = num01 + num02
+
+print("A soma é:", soma)
